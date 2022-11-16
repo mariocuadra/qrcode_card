3881f2a7e77fe9767492ba0b7796057c12ba23a8
Mario Cuadra Seguel <38959687+mariocuadra@users.noreply.github.com>
2022-11-16 16:03:55 -0300

    QrCode Changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,17 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qrcode_card/models/http/models_user.dart';
+import 'package:qrcode_card/service/local_storage.dart';
 import 'package:qrcode_card/shared/custom_input.dart';
-import 'package:qrcode_card/shared/custom_qrcode.dart';
+import 'api/card_ubo_api.dart';
+import 'dart:html' as html;
 
-void main() => runApp(MyApp());
+void main() async {
+  await LocalStorage.configurePrefs();
+  CardUboApi.configureDio();
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -16,16 +23,27 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late String data = '';
+  String data = '';
+  String emailuser = '';
+  String idpersona ="0"; 
+  String nombre ="nombre";
+  String appaterno ="apellido";
+  String cargo ="cargo";
+  String direccion ="direccion";
+  
+
+
+  String urlhost ='http://localhost:64053';
+  List<ModelUser> users = [];
   TextEditingController nameController = TextEditingController();
   late QrPainter _painter;
   GlobalKey globalKey = GlobalKey();
-  int size = 5;
-
+  int size = 1;
+  
+ 
   @override
   Widget build(BuildContext context) {
-
-     _painter = QrPainter(
+    _painter = QrPainter(
       errorCorrectionLevel: QrErrorCorrectLevel.H,
       eyeStyle: const QrEyeStyle(
         eyeShape: QrEyeShape.square,
@@ -34,7 +52,7 @@ class _MyAppState extends State<MyApp> {
       data: json.encode(data),
       version: QrVersions.auto,
     );
-    
+
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
@@ -44,28 +62,87 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: <Widget>[
             CustomPaint(
-              size: Size.square((size * 100).toDouble()),
-              key: globalKey,
-              painter: _painter),
-                  
+                size: Size.square((size * 100).toDouble()),
+                key: globalKey,
+                painter: _painter),
             CustomInput(
               nameController: nameController,
             ),
-            
             TextButton(
-              child: const Text('Boton'),
+              child: const Text('QR'),
               onPressed: () {
-               // print(nameController.text);
+                // print(nameController.text);
+
                 setState(() {
-                  data=nameController.text;
-            
-                });    
-              
+                  emailuser = nameController.text;
+                });
+
+                CardUboApi.httpGet('findperson/$emailuser').then((jsonString) {
+                 users = [];
+
+                  for (var u in jsonString) {
+                    ModelUser user = ModelUser(
+                        id: u["idcard"],
+                        nombre: u["nombre"],
+                        appaterno: u["appaterno"],
+                        cargo: u["cargo"],
+                        direccion: u["direccion"],
+                        telefono: u["telefono"],
+                        email: u["email"]);
+                    users.add(user);
+
+                    idpersona =users[0].id;
+                    nombre=users[0].nombre;
+                    appaterno=users[0].appaterno;
+                    cargo = users[0].cargo;
+                    direccion=users[0].direccion;
+
+
+                    print(users[0].id);
+
+                  }
+
+                  if (users.isEmpty) {
+                    setState(() {
+                      data = 'invalido';
+                      print(data);
+                    });
+                  } else {
+                    setState(() {
+                      String url ='http://localhost:58853/?para1=$idpersona&para2=$emailuser&para3=$nombre&para4=$appaterno&para5=$cargo&para6=$direccion';
+                      data = url;
+                     
+                      
+                    });
+                  }
+                }).catchError((e) {
+                  print('error en: $e');
+                });
               },
             ),
+              TextButton(
+              child: Text("CARD"),
+              onPressed: () { 
+
+                  openLink(users[0].id,users[0].email, users[0].nombre);
+
+         
+               },
+               
+              
+              ),
+
+
           ],
         ),
       ),
     );
+  }
+
+  void openLink( String para1, String para2, String para3) {
+    String url = Uri.encodeFull('http://localhost:51539/?para1=$idpersona&para2=$emailuser&para3=$nombre&para4=$appaterno&para5=$cargo&para6=$direccion');
+
+
+    html.window.open(url, '_blank');
   }
 }
--- lib/shared/custom_qrcode.dart
+++ lib/shared/custom_qrcode.dart
@@ -1,27 +1,5 @@
 
-import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
 
-class CustomQR extends StatefulWidget {
-
-  CustomQR({super.key, required this.datacode});
+class ClassName {
   
-  String datacode;
-  
-  @override
-  State<CustomQR> createState() => _CustomQRState();
-  
-}
-
-class _CustomQRState extends State<CustomQR> {
-  @override
-  Widget build(BuildContext context) {
-    return QrImage(
-        data: widget.datacode,
-        version: QrVersions.auto,
-        size: 200.0,
-        gapless: true,
-        
-      );
-  }
 }
